b375ec7105370ab4c1bb3d4cb2f5989f205c58fc
harbey33 <c1023762@my.shu.ac.uk>
2022-11-09 10:58:52 +0000

    bgcol

--- js/main.js
+++ js/main.js
@@ -2,4 +2,24 @@
     document.getElementById("myHeading").innerHTML = "Harvey";
     let currentNav = document.querySelector("nav ul li")
     currentNav.setAttribute("class", "currentPage")
+    // document.querySelector("nav ul li a").setAttribute("href", "https://www.google.co.uk")
+    let eventBtn = document.getElementById("myTestEvent")
+    eventBtn.addEventListener("click", myEvent)
+    function myEvent(){
+        //event handler
+        console.info("I was clicked")
+    }
+    
+    //bg colour selector
+
+    let colourBtns = document.querySelectorAll(".colPicker")
+    for (let i=0; i<colourBtns.length; i++){
+        colourBtns[i].addEventListener("click", chgColour)
+    }
+
+    function chgColour(ev){
+        console.info(ev.target.classList[0])
+        let newBgColour = ev.target.classList[0] + "Back"
+        document.querySelector("body").setAttribute("class", newBgColour)
+    }
 })()
