16662f359c576e02a89ca835b625d3baeccdd566
harbey33 <c1023762@my.shu.ac.uk>
2022-11-09 09:57:03 +0000

    hover

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 (function(){
-    document.getElementById("myHeading").innerHTML = "Harvey"
+    document.getElementById("myHeading").innerHTML = "Harvey";
+    let currentNav = document.querySelector("nav ul li")
+    currentNav.setAttribute("class", "currentPage")
 })()
